a8d94d55577af94ed7f14fdac33994ebf594746a
Mdzahin <afsarzahin@gmail.com>
2021-07-22 14:38:14 +0600

    Added phone call

--- components/contactButtons.js
+++ components/contactButtons.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
-import {Button, Icon} from '@ui-kitten/components';
+import {View, StyleSheet, Linking} from 'react-native';
+import {Button, Icon, Modal, Card} from '@ui-kitten/components';
 
 const MessageIcon = props => <Icon {...props} name="message-square" />;
 const CallIcon = props => <Icon {...props} name="phone-call" />;
 
 function ContactButtons({navigation}) {
+  const [visible, setVisible] = React.useState(false);
   return (
     <View style={{marginTop: 10, width: '100%'}}>
       <View style={styles.socialBtn}>
@@ -18,9 +19,37 @@ function ContactButtons({navigation}) {
         </Button>
       </View>
       <View style={styles.socialBtn}>
-        <Button style={styles.button} status="info" accessoryRight={CallIcon}>
-          Live Call
-        </Button>
+        {!visible ? (
+          <Button
+            style={styles.button}
+            onPress={() => setVisible(true)}
+            status="info"
+            accessoryRight={CallIcon}>
+            Live Call
+          </Button>
+        ) : (
+          <View
+            style={{
+              flexDirection: 'row',
+              width: '100%',
+              justifyContent: 'space-between',
+            }}>
+            <Button
+              status="info"
+              style={{width: '49%'}}
+              onPress={() => Linking.openURL('tel: 01819459974')}>
+              Call Offline
+            </Button>
+            <Button
+              status="info"
+              style={{width: '49%'}}
+              onPress={() =>
+                Linking.openURL('whatsapp://send?phone=+8801819459974')
+              }>
+              Call Online
+            </Button>
+          </View>
+        )}
       </View>
     </View>
   );
